b0a8229e7e889851d10e1cb61dac8834a1686983
Ranjeet101 <94225907+Bot-1-0-1@users.noreply.github.com>
2023-02-25 13:00:06 +0530

    Update index.jsx
    Optional Chaning required

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = `${user?.firstName} ${user?.lastName}`;                  // Created optional chain here So as it will display content dispite undefined or null properties 
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
