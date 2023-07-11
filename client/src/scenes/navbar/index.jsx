5ecaa5cb86de29b7e29d35ff97c63a4e960b3ede
honeybadgerng <124494961+honeybadgerng@users.noreply.github.com>
2023-07-11 08:40:24 +0000

    updated app name

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Yarn
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
